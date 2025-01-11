33a472fc8abc84e6ef346b5ff8b3084fed61994f
sangsil <siro9866@gmail.com>
2025-01-11 20:52:52 +0900

    하자하자

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
-
-            System.out.println("Hello, World23123!");
+        System.out.println("Hello, 1World23123!");
+        System.out.println("Hello, 2World23123!");
+        System.out.println("Hello, 3World23123!");
+        System.out.println("Hello, 4World23123!");
 
     }
 }
